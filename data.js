1f16820d48d531141634922c7555aa553d3e4e89
Munaza Malik <44956@students.riphah.edu.pk>
2025-02-21 14:52:23 +0500

    common js module

---
+++ data.js
@@ -0,0 +1,11 @@
+// data.js
+function getData() {
+    return [
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 }
+    ];
+}
+
+// Export the function using CommonJS
+module.exports = getData;
