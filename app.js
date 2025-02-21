cb5c3cb3fb7791f4d40984ba85cc2b0c2d32e8cf
Munaza Malik <44956@students.riphah.edu.pk>
2025-02-21 14:56:01 +0500

    common js module

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+const getData = require('./data'); // Import the function
+
+const result = getData(); // Call the function to get the array of objects
+
+console.log(result); // Display the data
